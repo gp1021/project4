e6d7f5079d5656304f3c66a5b11ae88c5e15b370
gp1021 <60611487+gp1021@users.noreply.github.com>
2020-05-22 11:12:56 -0400

    Update LabMinotaur.java

--- MazeV2/src/maze2/LabMinotaur.java
+++ MazeV2/src/maze2/LabMinotaur.java
@@ -2,19 +2,24 @@ package maze2;
 
 
 public class LabMinotaur {
+	//instance variables
 	private LabPosition _curPos;
 	private boolean _facingLeft = true;
 	private int[][] _starts = {{8, 4, 6, 8, 4},
 							   {1, 4, 9, 1, 1}};
-	
+	//getter method
 	public LabPosition getPos() {
 		return _curPos;
 	}
 	
+	//constructor
+	//creates a minotaur at the starting position previously decided on based on what level the player is on
 	public LabMinotaur(LabDesign z, int lvl){
 		_curPos = new LabPosition(getMinotaurStart(lvl).getRow(), getMinotaurStart(lvl).getCol());
 	}
 	
+	//basic move function
+	//moves the minotaur towards the position of the player
 	public void move(LabDesign z, int lvl){
 		if(_curPos.getCol() - 1 >= 0 && !z.getLab(lvl)[_curPos.getRow()][_curPos.getCol() - 1] && _facingLeft){
 			_curPos = new LabPosition(_curPos.getRow(), _curPos.getCol()-1);;
@@ -26,6 +31,9 @@ public class LabMinotaur {
 			_facingLeft = !_facingLeft;
 		}
 	}
+	
+	//more advanced move function
+	//checks for more factors when moving towards the player, based on row and column distances
 	public void hardMove(LabDesign z, LabPlayer p, int lvl){
 		int distRow = _curPos.getRow() - p.getPos().getRow();
 		int distCol = _curPos.getCol() - p.getPos().getCol();
@@ -86,6 +94,7 @@ public class LabMinotaur {
 		}
 	}
 	
+	//gets the start position of the minotaur based on what level the player is on, used in constructor
 	public LabPosition getMinotaurStart(int lvl) {
 		int x = _starts[0][lvl];
 		int y = _starts[1][lvl];
