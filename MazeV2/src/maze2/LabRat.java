9de6318ea09e90f11436dac3c758ec51228ce7f6
gp1021 <60611487+gp1021@users.noreply.github.com>
2020-05-22 14:53:39 -0400

    Update LabRat.java

--- MazeV2/src/maze2/LabRat.java
+++ MazeV2/src/maze2/LabRat.java
@@ -1,25 +1,29 @@
 package maze2;
 
 public class LabRat {
+	//instance variables
 	private LabPosition _curPos;
 	private boolean _facingLeft = true;
 	private int[][] _starts = {{1, 1, 5, 6, 8},
-							   {7, 4, 1, 1, 3}};
-	
+				   {7, 4, 1, 1, 3}};
+	//constructor
+	//puts the rat on the maze based on a position previously decided for the level
 	public LabRat(LabDesign z, int lvl) {
 		_curPos = new LabPosition(getRatStart(lvl).getRow(), getRatStart(lvl).getCol());
 	}
 	
+	//getter methods
 	public LabPosition getPos() {
 		return _curPos;
 	}
-	
+	//gets the specific start position for the rat based on the level, for the constructor
 	public LabPosition getRatStart(int lvl) {
 		int x = _starts[0][lvl];
 		int y = _starts[1][lvl];
 		return new LabPosition(x, y);
 	}
 	
+	//moves the rat from side to side in the same row it spawns in
 	public void move(LabDesign l, int lvl){
 		if(_curPos.getCol() - 1 >= 0 && !l.getLab(lvl)[_curPos.getRow()][_curPos.getCol() - 1] && _facingLeft){
 			_curPos = new LabPosition(_curPos.getRow(), _curPos.getCol()-1);
