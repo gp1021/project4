6229e77523717283e4f3d569c1da645e1fc71284
gp1021 <60611487+gp1021@users.noreply.github.com>
2020-05-22 14:56:29 -0400

    Update MainMazeTwo.java

--- MazeV2/src/maze2/MainMazeTwo.java
+++ MazeV2/src/maze2/MainMazeTwo.java
@@ -1,6 +1,6 @@
 package maze2;
 
-//allows the HLuser class to run and bring up the jpane
+//allows the UserLabyrinth class to run and bring up the jpane
 public class MainMazeTwo {
 	public static void main(String[] args) {
 		Lab3();
