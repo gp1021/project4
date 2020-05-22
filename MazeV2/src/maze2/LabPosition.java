82ad2e6e2df5e4e5c557a991372973721042b47d
gp1021 <60611487+gp1021@users.noreply.github.com>
2020-05-22 13:35:47 -0400

    Update LabPosition.java

--- MazeV2/src/maze2/LabPosition.java
+++ MazeV2/src/maze2/LabPosition.java
@@ -1,12 +1,18 @@
 package maze2;
 
 public class LabPosition {
+	//instance variables
 	private int _r;
 	private int _c;
+	
+	//constructor
+	//creates a position object that contains the row and column of an object
 	public LabPosition(int row, int col) {
 		_r = row;
 		_c = col;
 	}
+	
+	//getter methods
 	public int getRow() { return _r; }
 	public int getCol() { return _c; }
 	
